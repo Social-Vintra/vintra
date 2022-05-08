7bd6b3a0c4061d088dde3be0b962791c97d84626
DevArthurSantos <arthursantos.s@hotmail.com>
2022-05-08 12:01:21 -0300

    feat/createGlobalStyle.js

---
+++ styles/GlobalStyles.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
+export const GlobalStyles = createGlobalStyle` 
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@500;700&display=swap');
+
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  text-decoration: none;
+  list-style: none;
+  outline: none;
+  font-family: 'Nunito', sans-serif;
+  font-weight: 700;
+}
+`;
