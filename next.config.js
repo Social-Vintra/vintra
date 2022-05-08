0b8f8bf458c6954906aaa0321174a7aafc466a49
DevArthurSantos <arthursantos.s@hotmail.com>
2022-05-08 12:09:48 -0300

    feat/images domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["google.com", "imgur.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
