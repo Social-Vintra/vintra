399c200be19cb8d2ddfb11ab1794439d716e171c
DevArthurSantos <arthursantos.s@hotmail.com>
2022-05-08 12:13:53 -0300

    fix/metadados

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <title>Social Vintra - Home</title>
         <link rel="icon" href="/favicon.ico" />
         <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
         <meta name="descripiton" content="Guild de Albion Online" />
       </Head>
